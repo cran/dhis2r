27f7cf5faff9c19f840efee71cd976fc2e1cd5a3
Robinson Amanyiraho <amanyiraho@gmail.com>
2023-02-15 09:20:02 +0000

    version 0.1.1

--- inst/doc/dhis2r.R
+++ inst/doc/dhis2r.R
@@ -59,9 +59,16 @@ dhis2_play_connection$get_metadata(endpoint = "periodTypes", fields = ":all")
 dhis2_play_connection$get_metadata(endpoint = "indicators")
 
 ## -----------------------------------------------------------------------------
-dhis2_play_connection$get_analytics(analytic = "FTRrcoaog83", #Accute Flaccid Paralysis (Deaths < 5 yrs)
-                                    org_unit =   c("ImspTQPwCqd"), #Sierra Leone (National level)
-                                    period = "LAST_12_MONTHS",
-                                    output_scheme = "NAME")
+# dhis2_play_connection$get_analytics(analytic = "s46m5MS0hxu",
+#                                     org_unit = c("O6uvpzGd5pu", "fdc6uOvgoji"),
+#                                     period = "202101",
+#                                     output_scheme = "NAME")
+
+
+## -----------------------------------------------------------------------------
+# dhis2_play_connection$get_analytics(analytic = "FTRrcoaog83", #Accute Flaccid Paralysis (Deaths < 5 yrs)
+#                                     org_unit =   c("ImspTQPwCqd"), #Sierra Leone (National level)
+#                                     period = "LAST_12_MONTHS",
+#                                     output_scheme = "NAME")
 
 
